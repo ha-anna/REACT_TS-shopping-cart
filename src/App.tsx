45180a35e547bec18a35ba77cf70d83107bdeb31
Ha Anna <83631167+ha-anna@users.noreply.github.com>
2024-03-26 01:41:09 +0900

    Remove Home

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { Routes, Route } from 'react-router-dom';
 import { Container } from 'react-bootstrap';
-import { Home } from './pages/Home';
 import { Store } from './pages/Store';
 import { About } from './pages/About';
 import { Navbar } from './components/Navbar';
@@ -14,9 +13,6 @@ function App() {
         <Routes>
           <Route
             path='/'
-            element={<Home />}></Route>
-          <Route
-            path='/store'
             element={<Store />}></Route>
           <Route
             path='/about'
